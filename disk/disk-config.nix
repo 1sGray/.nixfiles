2d8b1ee2516e8cf62ca087affb9d64043e0a756b
gray <1s_gray@proton.me>
2024-08-25 18:20:29 +0400

    Added disk formatting with disko

---
+++ disk/disk-config.nix
@@ -0,0 +1,49 @@
+# USAGE in your configuration.nix.
+# Update devices to match your hardware.
+# {
+#  imports = [ ./disko-config.nix ];
+#  disko.devices.disk.main.device = "/dev/sda";
+# }
+{
+  config.disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "<disk-name>";
+        content = {
+          type = "gpt";
+          partitions = {
+            #boot = {
+            # size = "1M";
+            # type = "EF02"; # for grub MBR
+            #};
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+            swap = {
+              size = "2G";
+              content = {
+                type= "swap";
+                resumeDevice = true;
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
