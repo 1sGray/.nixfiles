8e34989dc1f0a56bd0e69b5473a235fdd043369a
gray <1s_gray@proton.me>
2024-08-26 23:11:33 +0400

    Commented some stuff from install.sh

--- install.sh
+++ install.sh
@@ -15,25 +15,25 @@ nix-shell -p git --command "git clone https://github.com/1sGray/.nixfiles$SCRIPT
 sudo nixos-generate-config --show-hardware-config > $SCRIPT_DIR/hardware-configuration.nix
 
 # Check if uefi or bios
-if [ -d /sys/firmware/efi/efivars ]; then
-    sed -i "0,/bootMode.*=.*\".*\";/s//bootMode = \"uefi\";/" $SCRIPT_DIR/flake.nix
-else
-    sed -i "0,/bootMode.*=.*\".*\";/s//bootMode = \"bios\";/" $SCRIPT_DIR/flake.nix
-    grubDevice=$(findmnt / | awk -F' ' '{ print $2 }' | sed 's/\[.*\]//g' | tail -n 1 | lsblk -no pkname | tail -n 1 )
-    sed -i "0,/grubDevice.*=.*\".*\";/s//grubDevice = \"\/dev\/$grubDevice\";/" $SCRIPT_DIR/flake.nix
-fi
+#if [ -d /sys/firmware/efi/efivars ]; then
+#    sed -i "0,/bootMode.*=.*\".*\";/s//bootMode = \"uefi\";/" $SCRIPT_DIR/flake.nix
+#else
+#    sed -i "0,/bootMode.*=.*\".*\";/s//bootMode = \"bios\";/" $SCRIPT_DIR/flake.nix
+#    grubDevice=$(findmnt / | awk -F' ' '{ print $2 }' | sed 's/\[.*\]//g' | tail -n 1 | lsblk -no pkname | tail -n 1 )
+#    sed -i "0,/grubDevice.*=.*\".*\";/s//grubDevice = \"\/dev\/$grubDevice\";/" $SCRIPT_DIR/flake.nix
+#fi
 
 # Patch flake.nix with different username/name and remove email by default
-sed -i "0,/gray/s//$(whoami)/" $SCRIPT_DIR/flake.nix
-sed -i "0,/gray/s//$(getent passwd $(whoami) | cut -d ':' -f 5 | cut -d ',' -f 1)/" $SCRIPT_DIR/flake.nix
-sed -i "s/1s_gray@proton.me//" $SCRIPT_DIR/flake.nix
-sed -i "s+~/.nixtfiles+$SCRIPT_DIR+g" $SCRIPT_DIR/flake.nix
+#sed -i "0,/gray/s//$(whoami)/" $SCRIPT_DIR/flake.nix
+#sed -i "0,/gray/s//$(getent passwd $(whoami) | cut -d ':' -f 5 | cut -d ',' -f 1)/" $SCRIPT_DIR/flake.nix
+#sed -i "s/1s_gray@proton.me//" $SCRIPT_DIR/flake.nix
+#sed -i "s+~/.nixtfiles+$SCRIPT_DIR+g" $SCRIPT_DIR/flake.nix
 
 # Open up editor to manually edit flake.nix before install
-if [ -z "$EDITOR" ]; then
-    EDITOR=nano;
-fi
-$EDITOR $SCRIPT_DIR/flake.nix;
+#if [ -z "$EDITOR" ]; then
+#    EDITOR=nano;
+#fi
+#$EDITOR $SCRIPT_DIR/flake.nix;
 
 # Permissions for files that should be owned by root
 #sudo $SCRIPT_DIR/harden.sh $SCRIPT_DIR;
